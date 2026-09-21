b1c139e496c257c8d44336900f9f3b2c7c17aa77
estheronakoya111-tech <estheronakoya111@gmail.com>
2026-09-21 21:38:57 +0100

    Set standard CORS to allow all origins

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                    "http://127.0.0.1:3000"],
-    
+    allow_origins=["*"],  # Allows requests from any frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
